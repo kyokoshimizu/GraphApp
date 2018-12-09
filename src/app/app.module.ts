538a10f049bd99121779c9ca7e059e8c04bfd5a3
kyokoshimizu <radio1117gaga@gmail.com>
2018-12-09 20:00:36 +0900

    Fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatTableModule, MatTabsModule, } from '@angular/material';
+import { Pattern1Component } from './pattern1/pattern1.component';
+import { Pattern2Component } from './pattern2/pattern2.component';
+import { Pattern3Component } from './pattern3/pattern3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pattern1Component,
+    Pattern2Component,
+    Pattern3Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTabsModule,
+    NgxChartsModule,
+  ],
+  exports: [
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
